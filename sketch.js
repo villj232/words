4d4bd40ff22ea0337e4afb5c95e3fdd76756cbde
villj232 <javiervillegas@javiers-MBP.wireless.newschool.edu>
2016-02-23 17:50:01 -0500

    classwork done

--- sketch.js
+++ sketch.js
@@ -1,13 +1,34 @@
 //Feel free to change these words:
 var nouns = ["Hammer","Cat","Tree", "Silence", "Umbrella","Sofa","Shirt","Jacket"];
+var articles = ["the", "a", "an"];
+var adjectives = ["solid", "empty", "strong", "intimidating", "wicked"];
+var colors = ["blue", "green", "yellow", "black", "red", "orange"];
+var myVars = [articles, adjectives, nouns, colors];
 
 
-function setup() {
+var randNum;
+
+function setup(){
 	createCanvas(windowWidth, windowHeight);
-	
+
+	randNum1 = floor(random(0,nouns.length));
+	randNum2 = floor(random(0,articles.length));
+	randNum3 = floor(random(0,adjectives.length));
+	randNum4 = floor(random(0, colors.length));
+
+
 }
 
 function draw(){
 	background("silver");
+	textFont("didot");
+	textSize(70);
+	fill(colors[randNum4]);
+
+	text(articles[randNum2]+ " "+adjectives[randNum3]+ " "+nouns[randNum1], 100,200)
+
+
+
+
 
 }
